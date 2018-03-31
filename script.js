e8a1d90410c0b87ec80cd1b12efaf885df84d03c
Mohsin Reshi <mohsin_reshi@yahoo.com>
2018-03-31 16:39:16 +0530

    add event listeners to all buttons | add resolution fnc

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-const container = document.querySelector('div');
-const gridSize = prompt('enter the size');
-for (let i = 0; i < gridSize * gridSize; i++) {
+const container = document.querySelector('.container');
+
+//default 16 x 16 grid
+for (let i = 0; i < 16 * 16; i++) {
   const gridDiv = document.createElement('div');
   container.appendChild(gridDiv);
-  gridDiv.style.height = 960 / gridSize + 'px';
-  gridDiv.style.width = 960 / gridSize + 'px';
-  gridDiv.setAttribute('class', 'grids');
+  gridDiv.style.height = 550 / 16 + 'px';
+  gridDiv.style.width = 550 / 16 + 'px';
+  gridDiv.classList.add('grids');
 }
 
-const grids = document.querySelectorAll('.grids');
-const grid = document.querySelector('.grids');
-
+const buttons = document.querySelectorAll('.color');
 const button = document.querySelector('.color');
-function changeBgColor(){
-  grids.forEach(grid => grid.addEventListener('mouseover', function() {
-    grid.style.backgroundColor = button.value;
-  }));
-}
-
-button.addEventListener('click', changeBgColor);
+const randomButton = document.querySelector('.random');
+const resetButton = document.querySelector('.reset');
+const resolutionButton = document.querySelector('.resolution');
 
 function randomColor() {
   const randomize = Math.random();
@@ -29,5 +24,46 @@ function randomColor() {
   else if (randomize > 0.5 && randomize <= 0.6) return 'yellow';
   else if (randomize > 0.6 && randomize <= 0.7) return 'orange';
   else if (randomize > 0.7 && randomize <= 0.8) return 'red';
-  else return 'black';
+  else return 'brown';
 }
+
+resolutionButton.onclick = function() {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+  let pixelSize = 1;
+  do {
+    pixelSize = prompt('Enter the pixel size (between 1 and 128px): ');
+  } while (pixelSize <= 0 || pixelSize > 128);
+  
+  for (let i = 0; i < pixelSize * pixelSize; i++) {
+    const gridDiv = document.createElement('div');
+    container.appendChild(gridDiv);
+    gridDiv.style.height = 550 / pixelSize + 'px';
+    gridDiv.style.width = 550 / pixelSize + 'px';
+    gridDiv.classList.add('grids');
+  }
+}
+
+
+buttons.forEach(button => button.addEventListener('click', function(){
+  const grids = document.querySelectorAll('.grids');
+  const grid = document.querySelector('.grids');
+  grids.forEach(grid => grid.addEventListener('mouseover', function() {
+    grid.style.backgroundColor = button.value;
+  }));
+}));
+
+resetButton.onclick = function() {
+  const grids = document.querySelectorAll('.grids');
+  const grid = document.querySelector('.grids');
+  grids.forEach(grid => grid.style.backgroundColor = 'white')
+};
+
+randomButton.onclick = function() {
+  const grids = document.querySelectorAll('.grids');
+  const grid = document.querySelector('.grids');
+  grids.forEach(grid => grid.addEventListener('mouseover', function() {
+    grid.style.backgroundColor = randomColor();
+  }))
+};
